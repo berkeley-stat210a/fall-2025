ee18321b58e757bd691e40f7efb58aae637c921a
Ryan Lovett <rylo@berkeley.edu>
2025-05-30 15:32:50 -0700

    Built site for gh-pages

--- send.sh
+++
@@ -1,5 +0,0 @@
-cp ../fall-2024-private/homework/homework*.tex homework/.
-cp ../fall-2024-private/homework/homework*.pdf homework/.
-cp ../fall-2024-private/homework/*.csv homework/.
-cp ~/Dropbox/Notability/210A-F24-Lectures/lecture* handwritten
-quarto publish gh-pages
